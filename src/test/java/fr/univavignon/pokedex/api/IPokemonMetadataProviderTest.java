b52a5df049cedd7f26296ce9acee9bb9c35c6d0d
Sabri.Boussetha <bsbabou@gmail.com>
2017-03-06 18:36:43 +0100

    add test directory to maven

---
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -0,0 +1,10 @@
+package fr.univavignon.pokedex.api;
+
+import org.junit.Test;
+
+public class IPokemonMetadataProviderTest {
+	@Test
+	public void firstTest() {
+		System.out.println("first Test");
+	}
+}
