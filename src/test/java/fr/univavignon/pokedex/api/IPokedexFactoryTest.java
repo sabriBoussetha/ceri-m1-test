7dd58a9d26d489f3cfd9230bc25125fcf0976c00
Alexandre.Latif <alex.latif4@gmail.com>
2017-03-29 18:24:05 +0200

    in progress

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -8,16 +8,21 @@ import org.junit.Test;
 public class IPokedexFactoryTest {
 	
 	protected IPokedexFactory iPokedexFactory;
-	
+	protected static IPokemonMetadataProvider iPokemonMetadataProvider;
 	@Before
 	protected void setUp() {
-		iPokedexFactory = mock(IPokedexFactory.class);// TODO mock ou instance
-		when(iPokedexFactory.createPokedex(null, null)).thenReturn(null);
+		setUpMock(this.iPokedexFactory);
+		IPokemonMetadataProviderTest.configureMoke(iPokemonMetadataProvider);
 	}
 	
 	@Test
 	public void firstTest() {
 		assertEquals(iPokedexFactory, null);
 		System.out.println("IPokedexFactoryTest.firstTest()");
+	}
+	
+	public static void setUpMock(IPokedexFactory iPokedexFactory){
+		iPokedexFactory = mock(IPokedexFactory.class);// TODO mock ou instance
+		when(iPokedexFactory.createPokedex(iPokemonMetadataProvider, null)).thenReturn(null);
 	}
 }
--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -2,19 +2,46 @@ package fr.univavignon.pokedex.api;
 
 import org.junit.Before;
 import org.junit.Test;
-import static org.mockito.Mockito.mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import static org.mockito.Mockito.*;
 
 public class IPokemonMetadataProviderTest {
 	
 	protected IPokemonMetadataProvider iPokemonMetadataProvider;
 	
 	@Before
-	protected void setUp(){
+	protected void setUp() throws PokedexException{
 		this.iPokemonMetadataProvider = mock(IPokemonMetadataProvider.class);
+		when(iPokemonMetadataProvider.getPokemonMetadata(anyInt())).thenAnswer(new Answer<PokemonMetadata>() {
+
+			@Override
+			public PokemonMetadata answer(InvocationOnMock invocation) throws Throwable {
+				return new PokemonMetadata(1, "PokeName", 50, 50, 50);
+			}
+			
+		});
 	}
 
 	@Test
 	public void firstTest() {
 		System.out.println("IPokemonMetadataProviderTest.firstTest()");
+	}
+	
+	public static void configureMoke(IPokemonMetadataProvider iPokemonMetadataProvider){
+		try {
+		iPokemonMetadataProvider = mock(IPokemonMetadataProvider.class);
+			when(iPokemonMetadataProvider.getPokemonMetadata(anyInt())).thenAnswer(new Answer<PokemonMetadata>() {
+
+				@Override
+				public PokemonMetadata answer(InvocationOnMock invocation) throws Throwable {
+					return new PokemonMetadata((int)invocation.getArguments()[0], "PokeName", 50, 50, 50);
+				}
+				
+			});
+		} catch (PokedexException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
