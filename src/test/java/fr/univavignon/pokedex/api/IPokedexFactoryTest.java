ff4ab6b3d51666251a44540cc7fda8787d4eb26d
Alexandre.Latif <alex.latif4@gmail.com>
2017-03-29 17:44:43 +0200

    in progress

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -1,10 +1,21 @@
 package fr.univavignon.pokedex.api;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+import org.junit.BeforeClass;
 import org.junit.Test;
-
 public class IPokedexFactoryTest {
+	
+	protected IPokedexFactory iPokedexFactory;
+	
+	@BeforeClass
+	protected void setUp() {
+		iPokedexFactory = mock(IPokedexFactory.class);// TODO mock ou instance
+		when(iPokedexFactory.createPokedex(null, null)).thenReturn(null);
+	}
 	@Test
 	public void firstTest() {
+		assertEquals(iPokedexFactory, null);
 		System.out.println("IPokedexFactoryTest.firstTest()");
 	}
 }
---
+++ src/test/java/fr/univavignon/pokedex/api/testImpl/PokedexFactoryTest.java
@@ -0,0 +1,13 @@
+package fr.univavignon.pokedex.api.testImpl;
+
+import org.junit.Before;
+import fr.univavignon.pokedex.api.IPokedexFactoryTest;
+
+public class PokedexFactoryTest extends IPokedexFactoryTest {
+	
+	@Before
+	protected void setUp() {
+		this.iPokedexFactory = null; // TODO récupérer la vrai instance ou le mock
+	}
+
+}
