a553db03c5003a786c44b7c150d69945da668cc0
Alexandre Latif <alex.latif4@gmail.com>
2017-05-07 22:37:54 +0200

    fini....

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -26,7 +26,7 @@ public class IPokedexTest {
 	public void sizeTest() {
 		int pIndex = iPokedex.size();
 		assertNotNull(pIndex);
-		assertEquals(pIndex, 2);
+		assertEquals(2, pIndex);
 	}
 	
 	@Test
@@ -91,7 +91,7 @@ public class IPokedexTest {
 	public void getAllPokemons(){
 		List<Pokemon> pokemons = iPokedex.getPokemons();
 		
-		assertEquals(3, pokemons.size());
+		assertEquals(2, pokemons.size());
 	}
 
 	public static IPokedex setUpMock() {
@@ -122,7 +122,7 @@ public class IPokedexTest {
 				}
 			});
 
-			when(iPokedex.size()).thenReturn(3);
+			when(iPokedex.size()).thenReturn(2);
 
 			when(iPokedex.getPokemonMetadata(4)).thenAnswer(new Answer<PokemonMetadata>() {
 
@@ -137,12 +137,12 @@ public class IPokedexTest {
 				@Override
 				public List<Pokemon> answer(InvocationOnMock invocation) throws Throwable {
 					Pokemon pokemon1 = new Pokemon(1, "Bulbasaur", 126, 126, 90, 40, 50, 60, 70, 55);
-					Pokemon pokemon2 = new Pokemon(4, "Charmeleon", 160, 140, 116, 54, 15, 95, 70, 55);
+//					Pokemon pokemon2 = new Pokemon(4, "Charmeleon", 160, 140, 116, 54, 15, 95, 70, 55);
 					Pokemon pokemon3 = new Pokemon(2, "Ivysaur", 156, 158, 120, 54, 95, 74, 194, 45);
 					
 					List<Pokemon> list = new ArrayList<>();
 					list.add(pokemon1);
-					list.add(pokemon2);
+//					list.add(pokemon2);
 					list.add(pokemon3);
 					
 					return list;
